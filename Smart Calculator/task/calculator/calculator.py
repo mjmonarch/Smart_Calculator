844025b1272c63c2146d0cce19c67bb554e20138
Sergii Kamenskyi <serg@apple.com>
2022-04-23 09:31:39 +0300

    stage 2 completed!

--- Smart Calculator/task/calculator/calculator.py
+++ Smart Calculator/task/calculator/calculator.py
@@ -1,3 +1,12 @@
 # -------------------------------------- STAGE 1 --------------------------------------
-args = [int(x) for x in input().split()]
-print(sum(args))
+# args = [int(x) for x in input().split()]
+# print(sum(args))
+
+# -------------------------------------- STAGE 2 --------------------------------------
+inp = input()
+while inp != '/exit':
+	if inp:
+		args = [int(x) for x in inp.split()]
+		print(sum(args))
+	inp = input()
+print("Bye!")
