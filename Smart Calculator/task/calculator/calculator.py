2685b97df53cfa85e3f36f26de04c041b06cedd4
Sergii Kamenskyi <serg@apple.com>
2022-04-23 09:33:47 +0300

    stage 3 completed!

--- Smart Calculator/task/calculator/calculator.py
+++ Smart Calculator/task/calculator/calculator.py
@@ -3,10 +3,22 @@
 # print(sum(args))
 
 # -------------------------------------- STAGE 2 --------------------------------------
+# inp = input()
+# while inp != '/exit':
+# 	if inp:
+# 		args = [int(x) for x in inp.split()]
+# 		print(sum(args))
+# 	inp = input()
+# print("Bye!")
+
+# -------------------------------------- STAGE 3 --------------------------------------
 inp = input()
 while inp != '/exit':
 	if inp:
-		args = [int(x) for x in inp.split()]
-		print(sum(args))
+		if inp == '/help':
+			print("The program calculates the sum of numbers")
+		else:
+			args = [int(x) for x in inp.split()]
+			print(sum(args))
 	inp = input()
 print("Bye!")
