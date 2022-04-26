7e72f049a9620f071c5e781370009c93a921fcff
Sergii Kamenskyi <serg@apple.com>
2022-04-26 13:32:50 +0300

    stage 5 completed!

--- Smart Calculator/task/calculator/calculator.py
+++ Smart Calculator/task/calculator/calculator.py
@@ -24,6 +24,46 @@
 # print("Bye!")
 
 # -------------------------------------- STAGE 4 --------------------------------------
+# def get_func(sequence):
+# 	func = sequence[0]
+# 	if len(sequence) > 1:
+# 		for elem in sequence[1:]:
+# 			if elem == '-':
+# 				if func == '-':
+# 					func = '+'
+# 				else:
+# 					func = '-'
+# 	if func == '-':
+# 		return lambda a, b: a - b
+# 	elif func == '+':
+# 		return lambda a, b: a + b
+#
+#
+# def do_action(num1, num2, func):
+# 	return func(num1, num2)
+#
+#
+# inp = input()
+# while inp != '/exit':
+# 	if inp:
+# 		if inp == '/help':
+# 			print("The program calculates the result of basic addict///subtract operation on numbers. -- means +")
+# 		else:
+# 			elements = inp.split()
+# 			result = int(elements[0])
+# 			for i, elem in enumerate(elements[1:], 1):
+# 				if i % 2 == 1:
+# 					func = get_func(elem)
+# 				else:
+# 					result = do_action(result, int(elem), func)
+# 			print(result)
+# 	inp = input()
+# print("Bye!")
+
+# -------------------------------------- STAGE 5 --------------------------------------
+import re
+
+
 def get_func(sequence):
 	func = sequence[0]
 	if len(sequence) > 1:
@@ -46,16 +86,24 @@ def do_action(num1, num2, func):
 inp = input()
 while inp != '/exit':
 	if inp:
-		if inp == '/help':
-			print("The program calculates the result of basic addict///subtract operation on numbers. -- means +")
-		else:
-			elements = inp.split()
-			result = int(elements[0])
-			for i, elem in enumerate(elements[1:], 1):
-				if i % 2 == 1:
-					func = get_func(elem)
+		if not re.match(r'/', inp):
+			if re.match(r'[+-]?\d+(\s+[-+]+\s+[+-]?\d+)*$', inp):
+				if inp == '/help':
+					print("The program calculates the result of basic addict///subtract operation on numbers. -- means +")
 				else:
-					result = do_action(result, int(elem), func)
-			print(result)
+					elements = inp.split()
+					result = int(elements[0])
+					for i, elem in enumerate(elements[1:], 1):
+						if i % 2 == 1:
+							func = get_func(elem)
+						else:
+							result = do_action(result, int(elem), func)
+					print(result)
+			else:
+				print("Invalid expression")
+		elif inp == '/help':
+			print("The program calculates the result of basic addict/subtract operation on numbers. -- means +")
+		else:
+			print("Unknown command")
 	inp = input()
 print("Bye!")
